a0568738cc5e3e6d72683507918d3cfcc148763b
Jim29800 <jbouvier1173@gmail.com>
2017-09-15 15:24:28 +0200

    couleur

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+var color = getRandomColor();
 $(function () {
     //demande à l'utilisateur de choisir un pseudo
     var pseudo = prompt('Quel est votre pseudo ?');
@@ -6,16 +15,17 @@ $(function () {
     }
     var socket = io();
     $('form').submit(function(){
-        socket.emit('chat message', $('#m').val());
+        socket.emit('chat message', color , pseudo , $('#m').val());
         $('#m').val('');
         return false;
     });
-    socket.on('chat message', function(msg){
-        $('#messages').append($('<li>').text(msg));
+    socket.on('chat message', function(color,pseudo,msg){
+        //'<strong style="color: ' + color + '";>' + pseudo + '</strong> : '+
+        $('#messages').append($('<li>').html('<strong style="color: ' + color + '";>' + pseudo + '</strong> : '+ msg));
     });
     // On demande le pseudo, on l'envoie au serveur et on l'affiche dans le titre
     //var pseudo = prompt('Quel est votre pseudo ?');
-    socket.emit('nouveau_client', pseudo);
+    socket.emit('nouveau_client', pseudo, color);
     //change l'onglet de la page
     document.title = pseudo + ' - ' + document.title;
 });
--- server.js
+++ server.js
@@ -11,23 +11,25 @@ app.get('/', function (req, res) {
 })
 //Lors de la connexion d'un utilisateur :
 io.on('connection', function(socket){
-    socket.on('nouveau_client', function(pseudo) {
+    socket.on('nouveau_client', function(pseudo, color) {
         pseudo = ent.encode(pseudo);
+        socket.color = color;
         socket.pseudo = pseudo;
         //Affiche à tous les utilisateur déjà connectés, le psuedo du nouvel utilisateur      
-        socket.broadcast.emit('chat message', pseudo + ' viens de se connecté');
+        socket.broadcast.emit('chat message', color,pseudo, pseudo + ' viens de se connecté');
         //Affiche à l'utilisateur courant un message à sa connexion
-        socket.emit('chat message', 'Vous êtes connecté en tant que : ' + pseudo);
+        socket.emit('chat message',color,pseudo, 'Vous êtes connecté en tant que : ' + pseudo);
     });
     //Permet l'affichage des messages envoyer par les utilisateurs
-    socket.on('chat message', function(msg){
+    socket.on('chat message', function(color, pseudo, msg){
+        pseudo = ent.encode(pseudo);
         msg =  ent.encode(msg)
-        io.emit('chat message', msg);
+        io.emit('chat message',color,pseudo, msg);
     });
     //Lorsque qu'un utilisateur ferme le chat :
     socket.on('disconnect', function(){
         //Affiche à tous les utilisateur connectés, qu'un utilisateur viens de se déconnecté
-        socket.broadcast.emit('chat message', socket.pseudo + ' viens de se déconnecté !');
+        socket.broadcast.emit('chat message', socket.color, socket.pseudo, ' viens de se déconnecté !');
     });
 });
 
